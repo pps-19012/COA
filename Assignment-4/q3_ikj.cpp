fff3e2f8c045ee6ae92de798521a3063ead6d6ec
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-04-20 03:50:39 +0530

    Assignment-4 added

---
+++ Assignment-4/q3_ikj.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#define N 128
+
+using namespace std;
+ 
+void multMat(int mat1[][N],
+              int mat2[][N],
+              int rslt[][N])
+{
+    int i = 0, j = 0, k = 0;
+    for (i = 0; i < N; i++) {
+        for (k = 0; k < N; k++) {
+            rslt[i][j] = 0;
+            for (j = 0; j < N; j++)
+                rslt[i][j] += mat1[i][k] * mat2[k][j];
+        }
+    }
+}
+
+int main()
+{
+    int rslt[N][N];
+    int mat1[N][N];
+    int mat2[N][N];
+    
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N; j++) 
+        {
+            
+            mat1[i][j] = rand()%100;
+            mat2[i][j] = rand()%100;
+        }
+    }
+
+    timespec start, end;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    multMat(mat1, mat2, rslt);
+    clock_gettime(CLOCK_REALTIME, &end);
+    long long seconds = end.tv_sec - start.tv_sec;
+    long long nanoseconds = end.tv_nsec - start.tv_nsec;
+    long double elapsed = seconds + nanoseconds*(long double)1e-9;
+
+    cout<<"\nTime taken by meat portion of the code: "<<elapsed<<" seconds\n";
+    return 0;
+}
---
+++ Assignment-4/q4_ikj.py
@@ -0,0 +1,33 @@
+from audioop import mul
+import random
+import time
+
+
+def mulMat(N):
+    rslt = [[0] * N] * N
+    mat1 = [[0] * N] * N
+    mat2 = [[0] * N] * N
+
+    for i in range(0, N):
+        for j in range(0, N):
+            mat1[i][j] = random.randint(0, 50)
+
+    for i in range(0, N):
+        for j in range(0, N):
+            mat2[i][j] = random.randint(0, 50)
+
+    start = time.time()
+    for i in range(0, N):
+        for k in range(0, N):
+            rslt[i][j] = 0
+
+            for k in range(0, N):
+                rslt[i][j] += mat1[i][k] * mat2[k][j]
+                
+    end = time.time()
+    programTime = end - start
+    return programTime
+
+
+n = int(input("Enter n(size of matrix): "))
+print('{0:1.7f}'.format(mulMat(n)))
